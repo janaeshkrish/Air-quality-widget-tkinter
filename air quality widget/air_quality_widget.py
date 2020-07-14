4df3a7cb43fc1b0205d7146787f8dced05d0e881
Janaeshwar K <54946830+janaeshkrish@users.noreply.github.com>
2020-07-14 11:53:25 +0530

    Update air_quality_widget.py

--- air quality widget/air_quality_widget.py
+++ air quality widget/air_quality_widget.py
@@ -8,27 +8,8 @@ import threading
 import os
 from time import sleep
 
-def google_map():
-    txt =' chromium-browser -incognito -kiosk --disable-infobars --no-sandbox road.html'
-    os.system(txt)
-
-
 def widget():
-    '''#To identify the current location using IP address using server
-    res = requests.get('https://ipinfo.io/')
-    data = res.json()
-    location = data['loc'].split(',')
-    #latitude = location[0]
-    #longitude = location[1]'''
-    '''
-    #getting lat and long from metta data
-    with open('meta.txt', 'r+') as f:
-        for line in f.readlines():
-            line=line
-    lat=line[330:339]
-    lng=line[349:358]
-    #print(lat,lng) 
-        '''
+    #provide your current locations latitude,longitude and key.
     link="http://api.breezometer.com/air-quality/v2/current-conditions?lat={}&lon={}&key=440655dfcb1d4099a8689788038fca95".format(12.9719,77.5937)
     res = requests.get(link)
     Air_quality= res.json()
@@ -73,8 +54,6 @@ def widget():
     root.overrideredirect(1)
     root.update()
     while True:
-        #sleep(1800)
-        #based on the latitude and longitude finding the air quality
         link="http://api.breezometer.com/air-quality/v2/current-conditions?lat={}&lon={}&key=440655dfcb1d4099a8689788038fca95".format(12.9719,77.5937)
         res = requests.get(link)
         Air_quality= res.json()
@@ -84,12 +63,13 @@ def widget():
         Category="Category - {}".format(Air_quality['data']['indexes']['baqi']['category'])
         
         dominant_pollutant="Dominant Pollutant - {} ".format(Air_quality['data']['indexes']['baqi']['dominant_pollutant'])
+        #condition to check weather the content is changed after some time.   
         if aqi != i:
             cate.set(Category)
             aqiStr.set(AQI)
             dominantStr.set(dominant_pollutant)
         root.update()
- # This function is used to display time on the label 
+#This function is used to display time on the label you can call this function if needed
 def time():
     string = strftime('%H:%M:%S') 
         # Styling the label widget clock
@@ -102,8 +82,6 @@ def time():
 
 if __name__ == '__main__':
     root = tk.Tk()
-    
-    #threading.Thread(target = google_map).start()
     widget()
     
     
